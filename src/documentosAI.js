2ee4891e5c57ef2da8eab6e5d3c1117821a5ece7
pjlkaw <pedrorochabispo@gmail.com>
2026-04-13 10:14:52 -0300

    organização do código

--- src/documentosAI.js
+++ src/documentosAI.js
@@ -6,7 +6,7 @@ import chalk from 'chalk'
 import inquirer from 'inquirer'
 import stringSimilarity from 'string-similarity'
 import readlineSync from 'readline-sync'
-
+// .env e Groq
 import dotenv from 'dotenv'
 dotenv.config()
 import Groq from 'groq-sdk' //AI
@@ -26,124 +26,136 @@ export async function documentosAI() { //================ORGANIZAR CÓDIGO E ADI
 
     //Anexo de arquivos
     if (opcao === 'Anexar') {
-        console.log(chalk.gray("- É importante que o anexo deva estar em ./docs ! \n"));
+        console.log(chalk.gray("- É importante que o anexo deva estar em ./docs !"));
 
         const input = readlineSync.question(chalk.cyanBright(
-            `
-            ${chalk.magenta("Comandos:")}
+            `           ${chalk.magenta("Comandos:")}
             /resume
             /prompt (--Indisponível--)
             /list (--Indisponível--)
             /end 
-            ..:
-            `
+            ${chalk.magenta('-----------')}
+            ${chalk.magenta('..:')}`
         ))
 
         if (input === "") {
             return
         }
 
-        if (input === "/end") {
+        else if (input === "/end") {
             return
         }
 
-        if (input === "/resume") {
-            resumeDoc()
+        else if (input === "/resume") {
+            const caminho = await escolheArquivo()
+            await resumeDoc(caminho)
+            async function resumeDoc(caminho) {
+                const promptUsuario = "resuma"
+                const resposta = await promptDoc(caminho, promptUsuario)
+                console.log(resposta)
+            }
         }
 
-        async function resumeDoc() {
-            console.log("\nArquivos da pasta atual\n",arquivosPasta);
+        
+    }
+    
+    //CRIAR ARQUIVO
+    else if (opcao == 'Criar') {
+        console.log('Indisponível');
+    }
+    
+    else if (input === "/free") {
+        return
+    }
 
-            const { arquivo } = await inquirer.prompt([
+
+
+
+    //FUNCTIONS ============
+
+    async function groqAI(content) {
+        const chatCompletion = await groq.chat.completions.create({
+            messages: [
                 {
-                    type: 'input',
-                    name: 'arquivo',
-                    message: `${chalk.magenta('Escolha o arquivo ..:')}`,
-                    choices: arquivosPasta
-                }
-            ]);
-    
-            let arquivoFinal = arquivo
-            let caminho = `./docs/${arquivoFinal}`
-            
-            //SUGESTÂO DE ARQUIVO
-            if (!fs.existsSync(caminho)) {
-                const match = stringSimilarity.findBestMatch(arquivo, arquivosPasta)
-                const sugestao = match.bestMatch.target
-                const score = match.bestMatch.rating
-                if (score > 0.4) {
-                    console.log('Arquivo não encontrado');
-                    console.log(`Você quis dizer: ${sugestao}?`)
-                    
-                    const { confirmar } = await inquirer.prompt([
-                        {
-                            type: 'confirm',
-                            name: 'confirmar',
-                            message: 'Usar este arquivo?',
-                            default: true
-                        }
-                    ]); 
-    
-                    if (confirmar) {
-                        arquivoFinal = sugestao
-                        caminho = `./docs/${arquivoFinal}`
-                    } 
-                    
-                }
-                else {
-                    console.log(chalk.red('Operação cancelada'))
-                    return
-                }
-            }
+                    role: "user",
+                    content: content
+                },
+            ],
+            model: "openai/gpt-oss-20b",
+        });
 
-            officeparser.parseOffice(caminho, (data, err) => {
+        return chatCompletion.choices[0]?.message?.content || "";
+    }
+    
+    //Lê o arquivo e responde com IA
+    async function promptDoc(caminho, prompt) {
+        return new Promise((resolve, reject) => {
+            officeparser.parseOffice(caminho, async (data, err) => {
                 if(err) {
                     console.log(chalk.red("Erro ao ler arquivo"));
-                    return
+                    return reject(err)
                 }
                 let textoData = ''
-    
+
+                //envia separadamente o texto para textoData
                 data.content.forEach(item => {
                     if (item.text) {
                         textoData += item.text + "\n"
                     }
                 });
-    
-                // console.log(textoData); //CONTEÚDO DO ARQUIVO
-    
-                groqAI("resuma em no máximo 2 parágrafos"  + " esse conteúdo: \n \n" + textoData)
+
+                //Prompt AI
+                const resposta = await groqAI(prompt  + "\n\nconteúdo:\n" + textoData)
+                resolve(resposta)
+
             })
-            
-        }
-
+        })
     }
 
+    async function escolheArquivo() {
+        console.log("\nArquivos da pasta atual:\n",arquivosPasta);
 
-    //CRIAR ARQUIVO
-    else if (opcao == 'Criar') {
-        console.log('Indisponível');
-    }
+        const { arquivo } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'arquivo',
+                message: `${chalk.magenta(`Escolha o arquivo ..:`)}`,
+                choices: arquivosPasta
+            }
+        ]);
 
-    function groqAI(content) {
+        let arquivoFinal = arquivo
+        let caminho = `./docs/${arquivoFinal}`
         
-        async function main() {
-            const chatCompletion = await getGroqChatCompletion();
-            // Print the completion returned by the LLM.
-            console.log(chatCompletion.choices[0]?.message?.content || "");
-        }
-        
-        async function getGroqChatCompletion() {
-            return groq.chat.completions.create({
-                messages: [
+        //SUGESTÂO DE ARQUIVO
+        if (!fs.existsSync(caminho)) {
+            const match = stringSimilarity.findBestMatch(arquivo, arquivosPasta)
+            const sugestao = match.bestMatch.target
+            const score = match.bestMatch.rating
+            if (score > 0.4) {
+                console.log('Arquivo não encontrado!');
+                console.log(`Você quis dizer: ${sugestao}?`)
+                
+                const { confirmar } = await inquirer.prompt([
                     {
-                        role: "user",
-                        content: `${content}`
-                    },
-                ],
-                model: "openai/gpt-oss-20b",
-            });
+                        type: 'confirm',
+                        name: 'confirmar',
+                        message: 'Usar este arquivo?',
+                        default: true
+                    }
+                ]); 
+
+                if (confirmar) {
+                    arquivoFinal = sugestao
+                    caminho = `./docs/${arquivoFinal}`
+                } 
+            }
+            else {
+                console.log(chalk.red('Operação cancelada'))
+                return
+            }
         }
-        main()
+        return caminho
     }
-    
+
 }
